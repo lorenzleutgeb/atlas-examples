f8337f45af721f22bcf6e32d3eff03c2d3255613
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-11-15 15:08:26 +0100

    Remove duplicates and TeX file

--- iter_append.ml
+++
@@ -1,9 +0,0 @@
-iter t =
-  match t with
-    | nil       -> nil
-    | (l, x, r) -> (iter l, x, iter r);
-
-append t u =
-  match t with
-    | nil       -> u
-    | (l, x, r) -> (l, x, append r u);
