2a2f183414b3cfbebe50970559ff0abea504391b
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2020-10-16 08:03:43 +0200

    Add Splay Heap del_min, annotations in comments.

--- SplayHeap.ml
+++ SplayHeap.ml
@@ -29,30 +29,40 @@ partition p t = match t with
           | (ta1, x, ta2) → (ta1, 0, (ta2, a, (tb, ab, tbc)))
 *)
 
-insert ∷ Ord α ⇒ α ⨯ α ⨯ Tree α → Tree α
+insert ∷ Ord α ⇒ α ⨯ α ⨯ Tree α (* | [ 1 ↦ 1, (1 0) ↦ 6, (0 2) ↦ 1 ] *) → Tree α (* | [ 1 ↦ 1, (0 2) ↦ 1 ] *)
 insert d x h = match partition d x h with
-  | leaf      → leaf
+  | leaf      → (leaf, x, leaf)
   | (l, y, r) → (l, x, r)
 
-partition ∷ Ord α ⇒ α ⨯ α ⨯ Tree α → Tree α
+
+partition ∷ Ord α ⇒ α ⨯ α ⨯ Tree α (* | [ 1 ↦ 1, (1 0) ↦ 3, (0 2) ↦ 2 ] *) → Tree α (* | [ 1 ↦ 1, (0 2) ↦ 1 ] *)
 partition d p t = match t with
-  | leaf           → (leaf, d, leaf)
+  | leaf           → leaf
   | (tab, ab, tbc) → if ab <= p
     then match tbc with
       | leaf → ((tab, ab, leaf), d, leaf)
       | (tb, b, tc) → if b <= p
         then match partition d p tc with
           | leaf          → leaf
-          | (tc1, x, tc2) → (((tab, ab, tb), b, tc1), d, tc2)
+          | (tc1, x, tc2) → (((tab, ab, tb), b, tc1), d, tc2) (* zag zag *)
         else match partition d p tb with
           | leaf          → leaf
-          | (tb1, x, tb2) → ((tab, ab, tb1), d, (tb2, b, tc))
+          | (tb1, x, tb2) → ((tab, ab, tb1), d, (tb2, b, tc)) (* zag zig *)
     else match tab with
       | leaf → (leaf, d, (leaf, ab, tbc))
       | (ta, a, tb) → if a <= p
         then match partition d p tb with
           | leaf          → leaf
-          | (tb1, x, tb2) → ((ta, a, tb1), d, (tb2, ab, tbc))
+          | (tb1, x, tb2) → ((ta, a, tb1), d, (tb2, ab, tbc)) (* zig zag *)
         else match partition d p ta with
           | leaf          → leaf
-          | (ta1, x, ta2) → (ta1, d, (ta2, a, (tb, ab, tbc)))
+          | (ta1, x, ta2) → (ta1, d, (ta2, a, (tb, ab, tbc))) (* zig zig *)
+
+del_min ∷ Ord α ⇒ Tree α (* | [ 1 ↦ 1, (1 0) ↦ 1, (1 1) ↦ 1, (0 2) ↦ 1 ] *) → Tree α (* | [ 1 ↦ 1, (0 2) ↦ 1 ] *)
+del_min t = match t with
+  | leaf          → leaf
+  | (tab, b, tc) → match tab with
+    | leaf → tc
+    | (ta, a, tb) → if ta == leaf
+      then (tb, b, tc)
+      else (del_min ta, a, (tb, b, tc))
