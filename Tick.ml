949501b772bb20a3c999ce2705ed461aa60d2522
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2022-01-10 16:17:14 +0100

    Tick: Reorganize

--- Tick.ml
+++ Tick.ml
@@ -1,21 +1,26 @@
-f ∷ Tree α → Tree α
-f t = let t1 ≔ ~ 1 2 t in t1
+f0 ∷ Tree α → Tree α
+f0 t = let t1 ≔ ~ 1 2 t in t1
 
-(**
- * NOTE: Terminates only on leaf, but does not terminate on any other Tree
- *)
-g ∷ Tree α → Tree α
-g t = match t with
-  | leaf → leaf
-  | t    → let t1 ≔ ~ 1 2 (g t) in t1
-
-(**
- * NOTE: Terminates.
- *)
-gl ∷ Tree α → Tree α
-gl t = match t with
+f1 ∷ Tree α → Tree α
+f1 t = match t with
   | leaf        → leaf
-  | (tl, x, tr) → let t1 ≔ ~ 1 2 (gl tl) in t1
+  | (tl, x, tr) → ~ 1 2 (f1 tl)
 
-h ∷ Tree α → Tree α
-h t = let t1 ≔ ~ 1 2 (h t) in t1
+f2 ∷ Tree α → Tree α
+f2 t = match t with
+  | leaf        → leaf
+  | (tl, x, tr) → let t1 ≔ ~ 1 2 (f2 tl) in t1
+
+(**
+ * NOTE: Terminates only on `leaf`, but does not terminate on any other tree!
+ *)
+f3 ∷ Tree α → Tree α
+f3 t = match t with
+  | leaf → leaf
+  | t    → let t1 ≔ ~ 1 2 (f3 t) in t1
+
+(**
+ * NOTE: Does not terminate.
+ *)
+f4 ∷ Tree α → Tree α
+f4 t = let t1 ≔ ~ 1 2 (f4 t) in t1
