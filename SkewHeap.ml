3bdd440690afeaa2f7cb2674887052cb2f2f375e
Lorenz Leutgeb <lorenz@mpi-inf.mpg.de>
2022-09-02 13:25:33 +0200

    Update SkewHeap.ml

--- SkewHeap.ml
+++ SkewHeap.ml
@@ -7,6 +7,16 @@
  *   Journal of Automated Reasoning, Vol. 62, Iss. 3, pp. 367-391
  *   https://doi.org/10.1007/s10817-018-9459-3
  *   https://dblp.org/rec/journals/jar/NipkowB19
+ *
+ * Nipkow and Brinkop use a potential function that counts the number
+ * of "right heavy" nodes:
+ *
+ *   rh l r = if |l| < |r|
+ *              then 1
+ *              else 0
+ *
+ *   Φ leaf         = 0
+ *   Φ (node l _ r) = Φ l + Φ r + rh l r
  *)
 
 insert ∷ Ord α ⇒ (α ⨯ Tree α) → Tree α
@@ -19,9 +29,9 @@ delete_min z h = match h with
 
 merge ∷ Ord α ⇒ (Tree α ⨯ Tree α) → Tree α
 merge h1 h2 = match h1 with
-  | leaf             → h2
-  | node h1l h1x h1r → match h2 with
-    | leaf             → (node h1l h1x h1r)
-    | node h2l h2x h2r → if h1x < h2x
-      then (node (~ merge (node h2l h2x h2r) h1r) h1x h1l)
-      else (node (~ merge (node h1l h1x h1r) h2r) h2x h2l)
+  | leaf          → h2
+  | node l1 a1 r1 → match h2 with
+    | leaf             → (node l1 a1 r1)
+    | node l2 a2 r2 → if a1 <= a2
+      then (node (~ merge (node l2 a2 r2) r1) a1 l1)
+      else (node (~ merge (node l1 x1 r1) r2) a2 l2)
